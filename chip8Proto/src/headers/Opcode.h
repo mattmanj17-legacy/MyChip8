975807fe9824afac4228629991d2fec0e10887e1
Matthew Draper <draper_matthewjohn@yahoo.com>
2014-11-03 22:08:36 -0800

    Reimpliment opcode index in assembly

--- chip8Proto/src/headers/Opcode.h
+++ chip8Proto/src/headers/Opcode.h
@@ -4,21 +4,26 @@
 #include <string>
 #include <sstream>
 #include <iostream>
-class Opcode{
+
+typedef unsigned short word;
+typedef unsigned char byte;
+
+class Opcode
+{
 private:
 	unsigned short code;
 public:
-	Opcode(unsigned short input)
+	Opcode(word input)
 	{
 		code=input;
 	}
 
 	// first-> the most significant Byte
 	// second-> the least significant Byte
-	Opcode(unsigned char first, unsigned char second)
+	Opcode(byte first, byte second)
 	{
-		unsigned short firstAsShort = (unsigned short)first;
-		unsigned short secondAsShort = (unsigned short)second;
+		word firstAsShort = (word)first;
+		word secondAsShort = (word)second;
 
 		firstAsShort <<= 8;
 		firstAsShort |= secondAsShort;
@@ -28,40 +33,84 @@ public:
 
 	// get nibble of opcode by index
 	// 0 -> most significant nibble : 3 -> least significant nible
-	unsigned char operator[](int index)
+	byte operator[](int index)
 	{
-		unsigned short nibble = code;
-
-		int bits = 4*(3-index);
-		unsigned short indexNibble = 0xf << bits;
+		/*
+			//example: code = 0x4H3A, index = 2
+			word nibble = code;
+			int bits = 4*(3-index);
+			word indexNibble = 0xf << bits;
+			//indexNibble: 0000000011110000
+			//or in hex : 0x0F00
 		
-		nibble &= indexNibble;
+			//   0x43HA
+			// & 0x00F0
+			//   ------
+			// = 0x00H0
+			nibble &= indexNibble;
 
-		//convert to char
-		nibble >>= bits;
-		return (unsigned char)nibble;
+			//convert to char
+			// 0x00H0 -> 0x0H
+			nibble >>= bits;
+			return (byte)nibble;
+		*/
+		word nibble = code;
+		__asm
+		{
+			//int bits = 4*(3-index);
+			//eax = 4*(3-index);
+			mov eax, 3
+			sub eax, index
+			mov ebx, 4
+			mul ebx
+
+			// int nibble = code
+			//ebx = code;
+			mov ebx, 0
+			mov bx, nibble
+
+			//int indexNibble = 0xf << edx
+			//edx = 0xf << eax;
+			mov edx, 000Fh
+			mov cl, al
+			shl edx, cl
+			
+			//nibble &= indexNibble;
+			//ebx &= edx;
+			and ebx, edx
+
+			//nibble >>= bits;
+			//ebx >>= edx;
+			shr ebx, cl
+
+			//return (byte)nibble;
+			//nibble = bx
+			mov nibble, bx
+		}
+		return nibble;
 	}
 
 	// Returns the opcode in one 2-Byte piece
-	unsigned short getCode(){
+	word getCode(){
 		return code;
 	}
 
 	// Returns the least significant Byte
-	unsigned char get8BitConstant(){
-		unsigned short toreturn;
+	byte get8BitConstant()
+	{
+		word toreturn;
 		__asm{
 			mov ecx, this
 			mov ax, [ecx]Opcode.code
 			and ax, 00FFh
 			mov toreturn, ax
 		}
-		return toreturn;
+		return (byte)toreturn;
 	}
 
 	// Returns the least significant Byte and a half
-	unsigned short get12BitConstant(){
-		unsigned short toreturn;
+	word get12BitConstant(){
+		word toreturn;
 		__asm{
 			mov ecx, this
 			mov ax, [ecx]Opcode.code
@@ -74,10 +123,10 @@ public:
 	// Returns the hexidecimal representation of the opcode
 	std::string toString(){
 		std::stringstream out;
-		out<< std::hex <<(int) this->operator[](0);
-		out<<std::hex<<(int) this->operator[](1);
-		out<<std::hex<<(int) this->operator[](2);
-		out<<std::hex<<(int) this->operator[](3);
+		out << std::hex << (int) this->operator[](0);
+		out << std::hex << (int) this->operator[](1);
+		out << std::hex << (int) this->operator[](2);
+		out << std::hex << (int) this->operator[](3);
 		std::string result;
 		out>>result;
 		return result;
